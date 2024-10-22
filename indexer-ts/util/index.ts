a05b4a6381f8d3556a0f56ffbc1b739025626c56
Matthew <matthew@urgero.net>
2024-10-22 14:09:25 -0500

    Fixes and cleanup (#2)
    * Migrate from old repository, dir change
    
    * Significant package cleanup, adjust `.gitignore`
    
    * `npm i @types/express@^4` and `npm audit fix`
    
    * bugfix: db missing profile ranking/rank counts
    
    A bug was introduced that removed the deconstructed `Profile` object
    from the profile database upserts. This was preventing the calculated
    `ranking`, `ranksPositive`, and `ranksNegative` properties from being
    upserted into the appropriate database fields. This commit fixes that,
    and also adjusts the schema so these fields are `NOT NULL`.
    
    * Add `prettier` + rc/ignore
    
    * `npm i typescript-eslint` and add `eslint.config.mjs`
    
    * Update `.gitignore` to keep `local_modules/`
    
    We are using a slightly modified version of the actively-maintained
    nanomsg package. Our version adjusts the npm `install` script to use
    the globally-installed version of `node-gyp` to work around a syntax
    error. Have not found an ideal solution to this yet.
    
    * Schema field name changes
    
    `value` => `sats`
    `ranksPositive/ranksNegative` => `votesPositive/votesNegative`
    
    * Prettier formatting + minor tweaks
    
    Renamed the `getScriptPartHex` method to `getScriptPart` and added
    an optional `encoding` parameter to return the `ScriptPart` bytes in the
    specified encoding.
    
    Also updated/removed some comments and whatnot. This commit is primarily
    for Prettier formatting.
    
    * Update package details + `rank-indexer-ts@0.0.1`
    
    Many of the details in `package.json` were outdated since migrating
    from the old repository. Those have been updated and the package version
    was set to 0.0.1 to revert in correct package version set in previous
    commit.
    
    * Mitigate vulnerabilities + add `.npmrc`
    
    Running `npm audit fix` revealed several severe vulnerabilities, many
    related to the local `nanomsg` package. This commit adds an `.npmrc`
    to the project to omit the installation of `devDependencies` by default.
    The `rank-indexer-ts` package dependenices were also adjusted to
    ensure the availability of required packages and modules for runtime.

--- indexer-ts/util/index.ts
+++
@@ -1,7 +0,0 @@
-/** */
-export type IndexerLogEntry = [ string, string ]
-export function log(entries: IndexerLogEntry[]) {
-  console.log(
-    `${new Date().toISOString()} ${entries.map(entry => entry.join('=')).join(' ')}`
-  )
-}
---
+++ lib/database.ts
@@ -0,0 +1,251 @@
+import { PrismaClient } from '@prisma/client'
+import { randomUUID } from 'crypto'
+import { Block, RankTransaction, Profile, ProfileMap } from './indexer'
+
+export default class Database {
+  private db: PrismaClient
+
+  constructor() {
+    this.db = new PrismaClient()
+  }
+
+  async connect() {
+    await this.db.$connect()
+  }
+
+  async disconnect() {
+    await this.db.$disconnect()
+  }
+  /**
+   *
+   * @param profileId
+   * @param platform
+   * @returns
+   */
+  async apiGetProfile(
+    profileId: string,
+    platform: string,
+  ): Promise<Partial<Profile> | undefined> {
+    try {
+      return await this.db.profile.findFirst({ where: { id: profileId } })
+    } catch (e) {
+      throw new Error(`isExistingProfile: ${e.message}`)
+    }
+  }
+  /**
+   *
+   * @param profiles
+   * @returns
+   */
+  async rewindProfiles(profiles: ProfileMap) {
+    try {
+      await this.db.$transaction(this.toProfileRewindStatements(profiles), {
+        isolationLevel: 'RepeatableRead',
+      })
+    } catch (e) {
+      throw new Error(`rewindProfiles: ${e.message}`)
+    }
+  }
+  /**
+   *
+   * @param profiles
+   */
+  async upsertProfiles(profiles: ProfileMap) {
+    try {
+      await this.db.$transaction(this.toProfileUpsertStatements(profiles), {
+        isolationLevel: 'RepeatableRead',
+      })
+    } catch (e) {
+      throw new Error(`upsertProfiles: ${e.message}`)
+    }
+  }
+  /**
+   *
+   * @param height
+   * @returns
+   */
+  async getRankTransactionsByHeight(
+    height: number,
+  ): Promise<RankTransaction[]> {
+    try {
+      return await this.db.rankTransaction.findMany({
+        where: { height },
+      })
+    } catch (e) {
+      throw new Error(`getRankTransactionsByHeight: ${e.message}`)
+    }
+  }
+  /**
+   *
+   * @param height
+   * @returns
+   */
+  async getBlockByHeight(height: number) {
+    try {
+      return await this.db.block.findFirst({
+        where: { height },
+      })
+    } catch (e) {
+      throw new Error(`getBlockByHeight: ${e.message}`)
+    }
+  }
+  /**
+   *
+   * @param height
+   */
+  async deleteBlockByHeight(height: number) {
+    try {
+      await this.db.block.delete({
+        where: { height },
+      })
+    } catch (e) {
+      throw new Error(`deleteBlockByHeight: ${e.message}`)
+    }
+  }
+  /**
+   *
+   * @param block
+   * @param txids
+   * @param profiles
+   */
+  async saveBlock(
+    block: Block,
+    rankTxids: Pick<RankTransaction, 'txid'>[],
+    profiles: Map<string, Profile>,
+  ) {
+    try {
+      await this.db.$transaction([
+        // Create the block and connect corresponding RANK txs
+        this.db.block.create({
+          data: {
+            ...block,
+            ranks: {
+              connect: rankTxids,
+            },
+          },
+        }),
+        // Upsert any profiles if necessary
+        // RANK txs upserted here will be connected to above block
+        ...this.toProfileUpsertStatements(profiles),
+      ])
+    } catch (e) {
+      throw new Error(
+        `saveBlock(${block.height}, ${rankTxids.length}, ${typeof profiles}): ${e.message}`,
+      )
+    }
+  }
+  /**
+   *
+   * @param blocks
+   * @param profiles
+   */
+  async saveBlockRange(blocks: Block[], profiles: ProfileMap) {
+    try {
+      await this.db.$transaction([
+        // Create all of the blocks first for `height` pkey
+        this.db.block.createMany({ data: blocks }),
+        // Upsert all profiles
+        // RANK txs upserted here are connected to above blocks
+        ...this.toProfileUpsertStatements(profiles),
+      ])
+    } catch (e) {
+      throw new Error(e.message)
+    }
+  }
+  /**
+   * Get the best `Block` from the database (i.e. highest `height`)
+   * @returns {Promise<Block>} The best `Block` as checkpoint
+   */
+  async getCheckpoint(): Promise<Block> {
+    try {
+      return await this.db.block.findFirst({
+        orderBy: { height: 'desc' },
+      })
+    } catch (e) {
+      throw new Error(`getCheckpoint: ${e.message}`)
+    }
+  }
+  /**
+   *
+   * @param profiles
+   * @returns
+   */
+  toProfileUpsertStatements(profiles: ProfileMap) {
+    const upserts: ReturnType<typeof this.db.profile.upsert>[] = []
+    for (const [profileId, profile] of profiles) {
+      upserts.push(
+        this.db.profile.upsert({
+          where: {
+            id_platform: {
+              id: profileId,
+              platform: profile.platform,
+            },
+          },
+          // profile doesn't exist
+          create: {
+            ...profile,
+            account: { create: { id: randomUUID() } },
+            ranks: {
+              createMany: { data: profile.ranks },
+            },
+          },
+          // profile exists
+          update: {
+            ranks: {
+              createMany: { data: profile.ranks },
+            },
+            ranking: {
+              increment: profile.ranking,
+            },
+            votesPositive: {
+              increment: profile.votesPositive,
+            },
+            votesNegative: {
+              increment: profile.votesNegative,
+            },
+          },
+        }),
+      )
+    }
+    return upserts
+  }
+  /**
+   *
+   * @param profiles
+   * @returns
+   */
+  toProfileRewindStatements(profiles: ProfileMap) {
+    const rewinds: ReturnType<typeof this.db.profile.update>[] = []
+    for (const [profileId, profile] of profiles) {
+      rewinds.push(
+        this.db.profile.update({
+          where: {
+            id_platform: {
+              id: profileId,
+              platform: profile.platform,
+            },
+          },
+          data: {
+            ranks: {
+              deleteMany: {
+                txid: {
+                  in: profile.ranks.map(rank => rank.txid),
+                },
+              },
+            },
+            ranking: {
+              decrement: profile.ranking,
+            },
+            votesPositive: {
+              decrement: profile.votesPositive,
+            },
+            votesNegative: {
+              decrement: profile.votesNegative,
+            },
+          },
+        }),
+      )
+    }
+    return rewinds
+  }
+}
