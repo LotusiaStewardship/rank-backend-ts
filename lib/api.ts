2ca23a114d134792b7c6f2911830baf9bfa1fed9
Matthew 🪷 <matthew@urgero.net>
2025-03-28 19:48:54 -0500

    Revert some config (#31)
    * Import `rank-lib` using `package.json`
    
    * Generate Prisma client into local `node_modules/.prisma`
    
    This allows us to generate the client from an external package and still
    use the `package.json` path to import the client.

--- lib/api.ts
+++ lib/api.ts
@@ -2,11 +2,7 @@ import { Express, Router, Request, Response, NextFunction } from 'express'
 import express from 'express'
 import Database from './database'
 import { API_SERVER_PORT } from '../util/constants'
-import {
-  PLATFORMS,
-  log,
-  type ScriptChunkPlatformUTF8,
-} from '../submodules/rank-lib'
+import { PLATFORMS, log, type ScriptChunkPlatformUTF8 } from 'rank-lib'
 import { Server } from 'http'
 import { EventEmitter } from 'events'
 
--- lib/database.ts
+++ lib/database.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from './prisma/client'
+import { PrismaClient } from '@prisma/client'
 import { randomUUID } from 'crypto'
 import { API_STATS_RESULT_COUNT, ERR } from '../util/constants'
 import type {
@@ -8,7 +8,7 @@ import type {
   ProfileMap,
   ScriptChunkPlatformUTF8,
   ScriptChunkSentimentUTF8,
-} from '../submodules/rank-lib'
+} from 'rank-lib'
 
 type RankStatistics = {
   ranking: bigint
