eeae73a78b4feba2fe1588b5964f2d9f1b434380
maff1989 <matthew@urgero.net>
2025-03-31 00:53:33 -0700

    Final change to Prisma generator output
    I finally figured it out! :)

--- lib/database.ts
+++ lib/database.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../prisma/prisma-client-js'
 import { randomUUID } from 'crypto'
 import { API_STATS_RESULT_COUNT, ERR } from '../util/constants'
 import type {
