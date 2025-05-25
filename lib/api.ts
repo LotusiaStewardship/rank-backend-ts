LotusiaStewardship/rank-backend-ts

commit 674e41e87ee509be58e5d8612f573e7fc6ae4a22

Add `/search` API endpoint, misc changes (#61)
This is a big one.

lib/api.ts
@@ -92,7 +92,14 @@ type AuthCacheEntry = {
 }
 /** Runtime cache of authenticated instances, where string is the instanceId*/
 type AuthCache = Map<string, AuthCacheEntry>
-type Endpoint = 'profile' | 'post' | 'stats' | 'instance' | 'wallet' | 'charts'
+type Endpoint =
+  | 'profile'
+  | 'post'
+  | 'stats'
+  | 'instance'
+  | 'wallet'
+  | 'charts'
+  | 'search'
 type EndpointHandler = (req: Request, res: Response) => void
 type EndpointParameter =
   | 'platform'
@@ -104,6 +111,7 @@ type EndpointParameter =
   | 'instanceId'
   | 'chartType'
   | 'dataType'
+  | 'searchType'
 /** This type is data returned from the database, not Temporal */
 type ChartWalletSummary = {
   /** Total number of votes cast */
@@ -128,6 +136,7 @@ type WalletRankActivityWorkflowResult = {
 }
 type Chart = 'wallet'
 type ChartData = 'summary' | 'activity'
+type SearchType = 'profile' | 'post'
 type EndpointParameterHandler = (
   req: Request,
   res: Response,
@@ -182,6 +191,9 @@ export default class API extends EventEmitter {
     this.router.param('pageNum', this.param.pageNum)
     this.router.param('scriptPayload', this.param.scriptPayload)
     this.router.param('instanceId', this.param.instanceId)
+    this.router.param('chartType', this.param.chartType)
+    this.router.param('dataType', this.param.dataType)
+    this.router.param('searchType', this.param.searchType)
     // Router GET endpoint configuration (DEEPEST ROUTES FIRST!)
     this.router.get(
       '/wallet/summary/:scriptPayload/:startTime?/:endTime?',
@@ -196,6 +208,7 @@ export default class API extends EventEmitter {
       '/stats/:statsRoute(profiles/[a-z-]+|posts/[a-z-]+)/:timespan?/:votes?/:pageNum?',
       this.get.stats,
     )
+    this.router.get('/search/:searchType/:query', this.get.search)
     this.router.get(
       '/:platform/:profileId/:postId/:scriptPayload',
       this.get.post,
@@ -352,6 +365,26 @@ export default class API extends EventEmitter {
         }
       }
       return { signature }
+    },
+    /**
+     * Validates a search type
+     * @param searchType - The search type to validate
+     * @returns The validated search type
+     */
+    searchType: (searchType: SearchType | undefined) => {
+      if (searchType === undefined) {
+        return {
+          error: 'search type must be specified',
+          statusCode: HTTP.BAD_REQUEST,
+        }
+      }
+      if (!['profile', 'post'].includes(searchType)) {
+        return {
+          error: 'invalid search type specified',
+          statusCode: HTTP.BAD_REQUEST,
+        }
+      }
+      return { searchType }
     },
   }
   /**
@@ -517,6 +550,30 @@ export default class API extends EventEmitter {
           )
       }
       req.params.dataType = dataType
+      next()
+    },
+    /**
+     *
+     * @param req
+     * @param res
+     * @param next
+     * @param searchType
+     */
+    searchType: async (
+      req: Request,
+      res: Response,
+      next: NextFunction,
+      searchType: SearchType,
+    ) => {
+      const validated = this.validate.searchType(searchType)
+      if (validated.error) {
+        return this.sendJSON(
+          res,
+          { error: validated.error },
+          validated.statusCode,
+        )
+      }
+      req.params.searchType = validated.searchType
       next()
     },
     /**
@@ -742,6 +799,50 @@ export default class API extends EventEmitter {
             { error: `invalid chart type specified` },
             HTTP.BAD_REQUEST,
           )
+      }
+    },
+    /**
+     *
+     * @param req
+     * @param res
+     * @returns
+     */
+    search: async (req: Request, res: Response) => {
+      const t0 = performance.now()
+      const query = req.params.query ?? ''
+      // if no query or query length is insufficient, return empty array
+      if (!query || query.length < 2) {
+        const t1 = (performance.now() - t0).toFixed(3)
+        log([
+          ['api', 'get.search'],
+          ...this.toLogEntries(req.params),
+          ['elapsed', `${t1}ms`],
+        ])
+        return this.sendJSON(res, [], HTTP.OK)
+      }
+      try {
+        const result = await this.db.apiSearchProfile(query)
+        const t1 = (performance.now() - t0).toFixed(3)
+        log([
+          ['api', 'get.search'],
+          ...this.toLogEntries(req.params),
+          ['elapsed', `${t1}ms`],
+        ])
+        return this.sendJSON(res, result, HTTP.OK)
+      } catch (e) {
+        const t1 = (performance.now() - t0).toFixed(3)
+        log([
+          ['api', 'error'],
+          ['action', 'get.search'],
+          ...this.toLogEntries(req.params),
+          ['message', `"${String(e)}"`],
+          ['elapsed', `${t1}ms`],
+        ])
+        return this.sendJSON(
+          res,
+          { error: 'search not found', params: req.params },
+          HTTP.NOT_FOUND,
+        )
       }
     },
     /**

lib/database.ts
@@ -1,7 +1,11 @@
 /* eslint-disable no-unsafe-finally */
 import { PrismaClient } from '../prisma/prisma-client-js'
 import { randomUUID } from 'crypto'
-import { API_STATS_RESULT_COUNT, ERR } from '../util/constants'
+import {
+  API_STATS_RESULT_COUNT,
+  API_SEARCH_RESULT_COUNT,
+  ERR,
+} from '../util/constants'
 import type {
   Block,
   RankTransaction,
@@ -12,6 +16,7 @@ import type {
   RankTarget,
   ScriptChunkPlatformUTF8,
   ScriptChunkSentimentUTF8,
+  IndexedRanking,
 } from 'rank-lib'
 
 type RankStatistics = Pick<
@@ -26,6 +31,11 @@ export type Timespan =
   | 'month'
   | 'quarter'
   | 'all'
+export type VoterDetails = {
+  ranking: string
+  votesPositive: number
+  votesNegative: number
+}
 export type ScriptPayloadActivitySummary = {
   scriptPayload: string
   totalVotes: number
@@ -304,6 +314,37 @@ export default class Database {
     })
   }
   /**
+   * Searches for profiles on any platform
+   * @param query - The query string to search for
+   * @returns An array of profiles matching the query
+   */
+  async apiSearchProfile(query: string): Promise<IndexedRanking[]> {
+    const searchResults: IndexedRanking[] = []
+    // execute the transaction, properly structuring the results
+    return await this.db.$transaction(async tx => {
+      const profiles = await tx.profile.findMany({
+        where: {
+          id: {
+            contains: query,
+            mode: 'insensitive',
+          },
+        },
+        take: API_SEARCH_RESULT_COUNT,
+      })
+      for (const profile of profiles) {
+        const data: IndexedRanking = {
+          platform: profile.platform,
+          profileId: profile.id,
+          ranking: profile.ranking.toString(),
+          votesPositive: profile.votesPositive,
+          votesNegative: profile.votesNegative,
+        }
+        searchResults.push(data)
+      }
+      return searchResults
+    })
+  }
+  /**
    * Retrieves profile information for a specific platform and profile ID
    * @param platform The platform identifier (ScriptChunkPlatformUTF8)
    * @param profileId The unique identifier of the profile
@@ -319,6 +360,8 @@ export default class Database {
       ranking: '0',
       votesPositive: 0,
       votesNegative: 0,
+      uniqueVoters: 0,
+      voterDetails: {},
     }
     return await this.db.$transaction(async tx => {
       try {
@@ -327,10 +370,17 @@ export default class Database {
             platform_id: { platform, id: profileId },
           },
         })
+        // get the voter details for the profile
+        const voterDetails = await this.getProfileVoterDetails(
+          tx as PrismaClient,
+          profileId,
+        )
         // Add indexed post data to return data
         data.ranking = profile.ranking.toString()
         data.votesPositive = profile.votesPositive
         data.votesNegative = profile.votesNegative
+        data.uniqueVoters = Object.keys(voterDetails).length ?? 0
+        data.voterDetails = voterDetails
       } catch (e) {
         // nothing to do here
       } finally {
@@ -1005,5 +1055,56 @@ export default class Database {
     for (const [id, post] of posts) {
       yield [id, post]
     }
+  }
+  /**
+   * Retrieves voter details for a specific platform profile
+   * @param tx - The Prisma client transaction
+   * @param platform - The platform identifier (ScriptChunkPlatformUTF8)
+   * @param profileId - The unique identifier of the profile
+   * @returns Object containing voter details for the profile
+   */
+  private async getProfileVoterDetails(tx: PrismaClient, profileId: string) {
+    const voters = await tx.rankTransaction.groupBy({
+      by: ['scriptPayload', 'sentiment'],
+      where: {
+        profileId,
+        AND: [{ profileId: { not: 'null' } }],
+      },
+      // How many votes total
+      // TODO: get total upvotes and downvotes
+      _count: {
+        sentiment: true,
+      },
+      _sum: {
+        sats: true,
+      },
+    })
+    const voterDetails: Record<string, VoterDetails> = {}
+    voters.forEach(details => {
+      let voter = voterDetails[details.scriptPayload]
+      if (!voter) {
+        voter = {
+          votesPositive: 0,
+          votesNegative: 0,
+          ranking: '0',
+        }
+      }
+
+      const ranking = BigInt(voter.ranking)
+      switch (details.sentiment) {
+        case 'positive':
+          voter.votesPositive += details._count.sentiment
+          voter.ranking = (ranking + details._sum.sats).toString()
+          break
+        case 'negative':
+          voter.votesNegative += details._count.sentiment
+          voter.ranking = (ranking - details._sum.sats).toString()
+          break
+      }
+
+      voterDetails[details.scriptPayload] = voter
+    })
+
+    return voterDetails
   }
 }

util/constants.ts
@@ -8,6 +8,7 @@ export const EXT_INSTANCE_ID_DIFFICULTY = 4
  */
 export const API_SERVER_PORT = 10655
 export const API_STATS_RESULT_COUNT = 5
+export const API_SEARCH_RESULT_COUNT = 5
 export const API_AUTH_CACHE_ENTRY_TTL = 420 // blocks over 1 day time span
 /**
  * NNG configuration

commit 07460ce2112fb751349c4f79a3351b1c8088b7d9

Bugfix: `ChartDataType` parameter missing `summary`
This commit fixes a bug introduced in 674e41e for the `summary` parameter of the `charts` API endpoint. The switch block that processed the `dataType` parameter was updated to include the `summary` case

lib/api.ts
@@ -134,8 +134,8 @@ type WalletRankActivityWorkflowResult = {
   /** Total amount of sats sent */
   totalPayoutAmount: number
 }
-type Chart = 'wallet'
-type ChartData = 'summary' | 'activity'
+type ChartType = 'wallet'
+type ChartDataType = 'summary' | 'activity'
 type SearchType = 'profile' | 'post'
 type EndpointParameterHandler = (
   req: Request,
@@ -511,7 +511,7 @@ export default class API extends EventEmitter {
       req: Request,
       res: Response,
       next: NextFunction,
-      chartType: Chart,
+      chartType: ChartType,
     ) => {
       switch (chartType) {
         case 'wallet':
@@ -537,10 +537,12 @@ export default class API extends EventEmitter {
       req: Request,
       res: Response,
       next: NextFunction,
-      dataType: ChartData,
+      dataType: ChartDataType,
     ) => {
       switch (dataType) {
         case 'activity':
+          break
+        case 'summary':
           break
         default:
           return this.sendJSON(
@@ -764,8 +766,8 @@ export default class API extends EventEmitter {
      */
     charts: async (req: Request, res: Response) => {
       const t0 = performance.now()
-      const chartType = req.params.chartType as Chart
-      const dataType = req.params.dataType as ChartData
+      const chartType = req.params.chartType as ChartType
+      const dataType = req.params.dataType as ChartDataType
       const startTime = (req.params.timespan ?? 'day') as Timespan
 
       switch (chartType) {